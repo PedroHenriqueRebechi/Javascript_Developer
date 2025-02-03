42048276ca85baee6de1481fcdfe1458129a80eb
Pedro Henrique Rebechi <pedrohenriquerebechi@gmail.com>
2025-02-03 18:18:06 -0300

    Commit

---
+++ Introducao/variaveis.js
@@ -0,0 +1,11 @@
+console.log('oi'); 
+
+let variavel = 10; // Alterável
+const pi = 3.14; // Constante
+
+console.log(variavel)
+
+variavel = 10/3;
+
+console.log(variavel.toFixed(2)) // Quantidade de casas decimais
+
