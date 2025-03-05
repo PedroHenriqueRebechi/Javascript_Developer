710604406c8a6a0481ba341a27c5b20adab28541
Pedro Henrique Rebechi <pedrohenriquerebechi@gmail.com>
2025-03-05 15:42:19 -0300

    Começando desafio details

--- Desenvolvimento Web/Projeto Pokedex/assets/js/main.js
+++ Desenvolvimento Web/Projeto Pokedex/assets/js/main.js
@@ -7,7 +7,7 @@ let offset = 0;
 
 function convertPokemonToLi(pokemon) {
     return `
-        <li class="pokemon ${pokemon.type}">
+        <li class="pokemon ${pokemon.type}" onclick="window.location.href='details.html'">
             <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
 
