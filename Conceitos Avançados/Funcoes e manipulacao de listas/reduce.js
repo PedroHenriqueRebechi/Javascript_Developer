d11607ef255a9b3d80ae50c367399779b75abc83
Pedro Henrique Rebechi <pedrohenriquerebechi@gmail.com>
2025-04-04 17:59:10 -0300

    função reduce

--- Conceitos Avançados/Funcoes e manipulacao de listas/reduce.js
+++ Conceitos Avançados/Funcoes e manipulacao de listas/reduce.js
@@ -1,1 +1,17 @@
 const lista = [1,2,3,4,5,6,7]
+
+// Reduz a lista a um unico valor
+
+// Exemplo: Quero somar os numeros da lista
+const soma = lista.reduce((previous, current) => {
+    return previous + current
+})
+
+console.log(soma)
+// 1 + 2
+// 3 + 3
+// 6 + 4
+// 10 + 5
+// 15 + 6
+// 21 + 7
+// 28
