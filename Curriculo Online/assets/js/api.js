7cdb4457ff445fa68c4c8e8e531d29e60a088ea7
Pedro Henrique Rebechi <pedrohenriquerebechi@gmail.com>
2025-06-19 17:18:45 -0300

    ajustando json

--- Curriculo Online/assets/js/api.js
+++ Curriculo Online/assets/js/api.js
@@ -1,3 +1,5 @@
-function fetchProfileData() {
-    const url = ''
+async function fetchProfileData() {
+    const url = 'https://raw.githubusercontent.com/PedroHenriqueRebechi/Javascript_Developer/refs/heads/main/Curriculo%20Online/assets/data/profile.json'
+    const fetching = await fetch(url)
+    return await fetching.json()
 }
